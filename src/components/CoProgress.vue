db5630af09254b1e20cf25ae6419bcbe33528b87
Ani <95martirosyan@gmail.com>
2022-02-16 22:06:34 +0400

    Fix bug in the package

---
+++ src/components/CoProgress.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="progress" :style="progress">
+    <div class="progress-value" :style="progressVal"></div>
+    <p class="text" :style="text" >{{percent}}%</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CoProgress',
+    data: function () {
+      return {
+
+      }
+    },
+    props: {
+      percent: {
+        required: true,
+        type: Number,
+        default: 0,
+      },
+      bgColor: {
+        required: false,
+        type: String,
+        default: 'white',
+      },
+      color: {
+        required: false,
+        type: String,
+        default: 'red',
+      },
+      showText: {
+        required: false,
+        type: Boolean,
+        default: true,
+      },
+      textColor: {
+        required: false,
+        type: String,
+        default: 'black',
+      },
+      textSize: {
+        required: false,
+        type: String,
+        default: 'larger',
+      },
+    },
+    mounted () {
+    },
+    computed: {
+      progressVal() {
+        return {
+          width: `${this.percent}%`,
+          background: this.color,
+        };
+      },
+      progress() {
+        return {
+          background: this.bgColor,
+        };
+      },
+      text() {
+        return {
+          color: this.textColor,
+          'font-size':this.textSize,
+          display: this.showText == 'false' ? 'none' : '',
+        };
+      }
+    }
+  }
+
+</script>
--- src/install.js
+++ src/install.js
@@ -1,16 +1,16 @@
-import Progress from "./components/CoProgress.vue";
+import CoProgress from "./components/CoProgress.vue";
 
-const ProgressBarSimple = {
+const CoProgressBar = {
     install(Vue) {
         // Let's register our component globally
         // https://vuejs.org/v2/guide/components-registration.html
-        Vue.component("Progress", Progress);
+        Vue.component("Progress", CoProgress);
     }
 };
 
 // Automatic installation if Vue has been added to the global scope.
 if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(ProgressBarSimple);
+    window.Vue.use(CoProgressBar);
 }
 
-export default ProgressBarSimple;
+export default CoProgress;
