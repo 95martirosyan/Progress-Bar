aa2bf99a61ac5408403f59187d6e2b01c618ea2b
Ani <95martirosyan@gmail.com>
2022-02-16 22:17:24 +0400

    Fix bug in the package

--- src/install.js
+++ src/install.js
@@ -1,16 +1,16 @@
-import CoProgress from "./components/CoProgress.vue";
+// import CoProgress from "./components/CoProgress.vue";
+//
+// const CoProgressBarSimple = {
+//     install(Vue) {
+//         // Let's register our component globally
+//         // https://vuejs.org/v2/guide/components-registration.html
+//         Vue.component("CoProgress", CoProgress);
+//     }
+// };
+//
+// // Automatic installation if Vue has been added to the global scope.
+// if (typeof window !== 'undefined' && window.Vue) {
+//     window.Vue.use(CoProgressBarSimple);
+// }
 
-const CoProgressBarSimple = {
-    install(Vue) {
-        // Let's register our component globally
-        // https://vuejs.org/v2/guide/components-registration.html
-        Vue.component("CoProgress", CoProgress);
-    }
-};
-
-// Automatic installation if Vue has been added to the global scope.
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(CoProgressBarSimple);
-}
-
-export default CoProgressBarSimple;
+export {default as CoProgress} from "./components/CoProgress.vue";
